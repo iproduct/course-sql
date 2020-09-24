98f07b175bb41075bdf25d74cf8ff17c31e3b9c2
Trayan Iliev <office@iproduct.org>
2020-09-24 21:08:50 +0300

    3 lab examples added

--- 03-sql-functions/01-sql-functions-lab.sql
+++ 03-sql-functions/01-sql-functions-lab.sql
@@ -14,5 +14,12 @@ SELECT INSERT(`title`, LOCATE('The', `title`, 1), 3, '***') AS 'Title'
 FROM `books` 
 WHERE SUBSTRING(`title`, 1, 3) = 'The';
 
-SELECT `title`, LOCATE('The', `title` COLLATE utf8mb4_0900_as_cs) AS 'Index of The' 
+SELECT `id`, `title`, 
+LOCATE('The', `title` COLLATE utf8mb4_0900_as_cs) AS 'Index of The' 
 FROM `books`;
+
+SELECT ROUND(SUM(`cost`), -2) AS `Total Cost` FROM `books`;
+
+SELECT CONCAT(`first_name`, ' ', `last_name`) AS `Name`, 
+TIMESTAMPDIFF(YEAR, `born`, IFNULL(`died`, NOW())) AS `Days Lived` 
+FROM `authors`;
