0ebfa0d0fa4ee58fb075855bbe325df7bae2554f
Trayan Iliev <office@iproduct.org>
2020-09-28 19:23:52 +0300

    aggregation examples added

--- 04-sql-aggregation/04-sql-lab.sql
+++ 04-sql-aggregation/04-sql-lab.sql
@@ -1,6 +1,7 @@
 USE `soft_uni`;
 
-SELECT `department_id`, ROUND(SUM(`salary`), 2) 
-AS `Salary Sum` FROM `employees`
+SELECT `department_id`, 
+		ROUND(SUM(`salary`), 2) AS `Salary Sum` 
+FROM `employees`
 GROUP BY `department_id`
 ORDER BY `department_id`;
